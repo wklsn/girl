2bf2b21eb47ee35c90e2bd25c3fe80eb844e3904
onlyor <onlyor@126.com>
2020-07-05 22:15:44 +0800

    增加单元测试

--- src/test/java/com/wkl/girl/GirlServiceTest.java
+++ src/test/java/com/wkl/girl/GirlServiceTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 测试service111
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GirlServiceTest {
